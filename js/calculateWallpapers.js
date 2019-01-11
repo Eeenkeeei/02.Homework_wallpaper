6de67e23f1bfa972e519c37b87670e3a254a4849
student <romanilichev@mail.ru>
2019-01-11 16:25:04 +0300

    fixed let in 2 string

--- js/calculateWallpapers.js
+++ js/calculateWallpapers.js
@@ -1,11 +1,10 @@
 export function calculateWallpapers(roomLenght, roomWidth, roomHight, wallpaperWidth, wallpaperLenght) {
-    let result;
     let perimeter=(roomLenght+roomWidth)*2;
     const delta=0.1; //  рекомендация по добавлению 10 см к высоте комнаты
     roomHight=roomHight+delta;
     let countStripes=wallpaperLenght/roomHight; // колво полос
     let countCanvas=perimeter/wallpaperWidth; // колво полотнищ
-    result=countCanvas/countStripes;
+    let result=countCanvas/countStripes;
     result = Math.round(result);
     return result;
 }
